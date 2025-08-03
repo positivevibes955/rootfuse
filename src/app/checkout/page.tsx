6854e8dd10a8e26b6c8459ac24643c5f5ba7ea5e
positivevibes955 <positivevibes955@gmail.com>
2025-08-02 20:19:47 -0500

    Fix:useState

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -37,7 +37,7 @@ const supabase = createClient();
 
         if (error) {
           console.error("Error getting user:", error);
-          setUser(null);
+          setUser(user as User | null);
           setLoading(false);
           return;
         }
